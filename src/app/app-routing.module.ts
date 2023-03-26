fa2b4d2d5bfadb128e24766ddab0462034b14fc9
AqeishParvez <aqeish.parvez@gmail.com>
2023-03-26 12:20:13 -0400

    Tag added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './content/about/about.component';
+import { ContactComponent } from './content/contact/contact.component';
+import { HomeComponent } from './content/home/home.component';
+import { ProjectsComponent } from './content/projects/projects.component';
+import { ServicesComponent } from './content/services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch:'full'},
+  {path: 'home', component: HomeComponent },
+  {path: 'about', component: AboutComponent },
+  {path: 'contact', component: ContactComponent },
+  {path: 'projects', component: ProjectsComponent },
+  {path: 'services', component: ServicesComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
