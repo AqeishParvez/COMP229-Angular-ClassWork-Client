c9d70d31ed90a86283296b8b2e9057e4ef0962d9
AqeishParvez <aqeish.parvez@gmail.com>
2023-03-31 01:12:48 -0400

    Movie add and edit completed

--- src/app/content/movies/add/add.component.ts
+++ src/app/content/movies/add/add.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
   selector: 'movie-add',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./add.component.css']
 })
 export class MovieAddComponent {
+
+  form: any = {
+    name: null,
+    year: null,
+    director: null,
+    genre: null,
+    runtime: null
+  }
+
+  isSuccessful = true;
+  errorMessage = "";
+
+  constructor(private movieService: MoviesService, private router: Router){}
+
+  onSubmit(){
+    this.movieService.addMovie(this.form)
+    .subscribe({
+      next: data => {
+        this.isSuccessful = true;
+        this.router.navigate(['/movie/list']);
+      },
+      error: err =>{
+        this.errorMessage = err.error.message;
+        this.isSuccessful = false;
+      }
+
+    })
+  }
+
+
+
+
 
 }
--- src/app/content/movies/edit/edit.component.ts
+++ src/app/content/movies/edit/edit.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
   selector: 'movie-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class MovieEditComponent {
+export class MovieEditComponent implements OnInit{
+
+
+  form: any = {
+    _id: null,
+    name: null,
+    year: null,
+    director: null,
+    genre: null,
+    runtime: null
+  }
+
+  isSuccessful = true;
+  errorMessage = "";
+
+  constructor(
+    private movieService: MoviesService,
+    private router: Router,
+    private route: ActivatedRoute
+    ){}
+
+  ngOnInit(): void {
+    this.route.params
+      .subscribe({
+        next: params =>{
+          this.form._id = params['id']; // :id
+
+          this.movieService.getMovie(this.form._id)
+            .subscribe({
+              next: data => {
+                this.form = data.movie;
+              },
+              error: err=> {
+                this.errorMessage = err.error.message;
+                this.isSuccessful = false;
+              }
+            })
+        },
+        error: err => {
+          this.errorMessage = err.error.message;
+          this.isSuccessful = false;
+        }
+      })
+  }
+
+
+
+  onSubmit(){
+    this.movieService.editMovie(this.form)
+    .subscribe({
+      next: data => {
+        this.isSuccessful = true;
+        this.router.navigate(['/movie/list']);
+      },
+      error: err =>{
+        this.errorMessage = err.error.message;
+        this.isSuccessful = false;
+      }
+
+    })
+  }
+
+
+
 
 }
